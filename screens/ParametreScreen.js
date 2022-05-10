23992f8f3c18a9edaac049b6f82918898056ca66
Arthur JORIS <ar.joris@laposte.net>
2022-05-10 14:55:44 +0200

    MAJ Parametres
    Mise à jour de la page Parametre avec l'intégration des boutons et du texte

--- screens/ParametreScreen.js
+++ screens/ParametreScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, Text, Switch } from 'react-native';
 import { getPokemon } from '../Api/PokeApi';
 import CustomItem from '../Components/Item';
 
@@ -7,41 +7,43 @@ import CustomItem from '../Components/Item';
 export default function HomeScreen(props) {
 
   const {navigation, ...restProps} = props
-
-  const [textParent, setTextParent] = useState();
-  const [listPokemon, setListPokemon] = useState("");
-  const [nextPage, setNextPage] = useState("https://pokeapi.co/api/v2/pokemon");
-
-  useEffect(() => {
-    loadPokemon(nextPage)
-  }, [])
-
-  const loadPokemon = (url) => {
-    getPokemon(url).then(datas => {
-      setListPokemon([...listPokemon, ...datas.results])
-      setNextPage(datas.next)
-    })
-  }
-
-  const renderItem = ({ item }) => (
-    <CustomItem url={item.url} name={item.name} navigation={navigation}></CustomItem>
-  );
+  const [isEnabledTop, setIsEnabledTop] = useState(false);
+  const [isEnabledBottom, setIsEnabledBottom] = useState(false);
+  const toggleSwitchTop = () => setIsEnabledTop(previousStateTop => !previousStateTop);
+  const toggleSwitchBottom = () => setIsEnabledBottom(previousStateBottom => !previousStateBottom);
  
   return(
     <>
     <View style={styles.container}>
+      <Text style={styles.title}>Donner l'accès aux fichiers</Text>
     </View>
-    <FlatList 
-      style={styles.list}
-      numColumns={3}
-      data={listPokemon} 
-      renderItem={renderItem} 
-      keyExtractor={item => item.name}
-      onEndReachedThreshold={0.5}
-      onEndReached={() => {
-        loadPokemon(nextPage)
-      }} 
-    />
+    <View style={styles.containerFlex}>
+      <Text style={styles.choiceNo}>Non</Text>
+      <Switch
+        trackColor={{ false: "#767577", true: "red" }}
+        thumbColor={isEnabledTop ? "lightcoral" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitchTop}
+        value={isEnabledTop}
+        style={styles.switch}
+      />
+      <Text style={styles.choiceYes}>Oui</Text>
+    </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Activer la rotation d'écran</Text>
+    </View>
+    <View style={styles.containerFlex}>
+      <Text style={styles.choiceNo}>Non</Text>
+      <Switch
+        trackColor={{ false: "#767577", true: "red" }}
+        thumbColor={isEnabledBottom ? "lightcoral" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitchBottom}
+        value={isEnabledBottom}
+        style={styles.switch}
+      />
+      <Text style={styles.choiceYes}>Oui</Text>
+    </View>
     </>
   )
   
@@ -51,6 +53,14 @@ const styles = StyleSheet.create({
 container: {
   justifyContent: 'center',
   alignItems: 'center',
+  marginTop:20,
+},
+containerFlex: {
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop:20,
+  display:'flex',
+  flexDirection:'row'
 },
 list: {
   marginLeft: 10,
@@ -58,4 +68,23 @@ list: {
   alignSelf: 'stretch',
   textAlign: 'center',
 },
+
+title: {
+  fontWeight:'bold',
+  fontSize:20,
+},
+choiceNo: {
+  fontWeight:'bold',
+  fontSize:15,
+  color:'#767577'
+},
+choiceYes: {
+  fontWeight:'bold',
+  fontSize:15,
+  color:'red'
+},
+
+siwtch: {
+width:20,
+}
 });
